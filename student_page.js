b292eb3380b61c92d3f9cf1e10af65a22dca1366
Nityam24 <nityampatel2402@gmail.com>
2023-02-06 19:15:42 +0530

    first commit

---
+++ student_page.js
@@ -0,0 +1,13 @@
+$("#menu-btn").click(function () {
+    $("#menu").toggleClass("active");
+  });
+
+//////////
+
+jQuery(document).ready(function(){
+
+    $('#just').click(function(){
+        window.print();
+        
+    })
+})
